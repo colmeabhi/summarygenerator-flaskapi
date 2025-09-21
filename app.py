399d334695a92fbd1b1d1edc44a2f753063cb262
abhi <ahirraoabhishek007@gmail.com>
2025-09-21 11:31:46 -0700

    added a demo front-end to the api for testing

--- app.py
+++ app.py
@@ -1,23 +1,29 @@
 # coding: utf-8
-from flask import Flask, request
+from flask import Flask, jsonify, render_template, request
+
 import model_features
 
 
-# ********************************************************************************************************************
-
 app = Flask(__name__)
 
+
+@app.route('/')
+def index():
+    """Serve a minimal UI for interacting with the summariser."""
+    return render_template('index.html')
+
+
 @app.route('/json-example', methods=['POST'])
-def Summary_Creator():
+def summary_creator():
     request_data = request.get_json()
-    text = None
-    
-    if request_data:
-        if 'textString' in request_data:
-            text = request_data['textString']
-            finalText = model_features.executeForAFile(text)
-     
-    return "The given text is : {}".format(finalText)
+    if not request_data or 'textString' not in request_data:
+        return jsonify({'error': 'textString is required'}), 400
+
+    text = request_data['textString']
+    final_text = model_features.executeForAFile(text)
+
+    return jsonify({'summary': final_text})
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
